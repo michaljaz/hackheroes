a7ee9869a95502b4ad8296de81d77ddf41002840
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2020-10-19 15:02:51 +0200

    Update Panel.js

--- senior-plus/src/Panel.js
+++ senior-plus/src/Panel.js
@@ -16,50 +16,49 @@ import AppIntroSlider from 'react-native-app-intro-slider'; // sliderek do tutor
 //import {slides,style} from './src/introSlider';
 
 //Zakładki
+
 import HomeScreen from './HomeScreen';
 import MedicineScreen from './MedicineScreen';
 import SettingsScreen from './SettingsScreen';
 import ContactScreen from './ContactScreen';
 
 const Tab = createBottomTabNavigator();
+
+
 export default function Panel(){
   return(
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="Dom"
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              if (route.name === 'Dom') {
-                return (
-                  <Ionicons name={'ios-home'} size={size} color={color}/>
-                );
-              }else if (route.name === 'Kontakt') {
-                return (
-                  <Ionicons name={'ios-text'} size={size} color={color}/>
-                );
-              } else if (route.name === 'Ustawienia') {
-                return (
-                  <Ionicons name={'ios-settings'} size={size} color={color} />
-                );
-              } else if (route.name === 'Leki') {
-                return (
-                  <Ionicons name={'ios-medkit'} size={size} color={color} />
-                );
-              }
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: '#DD0000',
-            inactiveTintColor: '#333333',
-          }}
-        >
-          <Tab.Screen name="Leki" component={MedicineScreen} options={{ tabBarBadge: null }} />
-          <Tab.Screen name="Dom" component={HomeScreen} options={{ tabBarBadge: null }} />
-          <Tab.Screen name="Kontakt" component={ContactScreen} options={{ tabBarBadge: null }} />
-          <Tab.Screen name="Ustawienia" component={SettingsScreen} options={{ tabBarBadge: null }} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <Tab.Navigator
+      initialRouteName="Dom"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === 'Dom') {
+            return (
+              <Ionicons name={'ios-home'} size={size} color={color}/>
+            );
+          }else if (route.name === 'Kontakt') {
+            return (
+              <Ionicons name={'ios-text'} size={size} color={color}/>
+            );
+          } else if (route.name === 'Ustawienia') {
+            return (
+              <Ionicons name={'ios-settings'} size={size} color={color} />
+            );
+          } else if (route.name === 'Leki') {
+            return (
+              <Ionicons name={'ios-medkit'} size={size} color={color} />
+            );
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#DD0000',
+        inactiveTintColor: '#333333',
+      }}
+    >
+      <Tab.Screen name="Leki" component={MedicineScreen} options={{ tabBarBadge: null }} />
+      <Tab.Screen name="Dom" component={HomeScreen} options={{ tabBarBadge: null }} />
+      <Tab.Screen name="Kontakt" component={ContactScreen} options={{ tabBarBadge: null }} />
+      <Tab.Screen name="Ustawienia" component={SettingsScreen} options={{ tabBarBadge: null }} />
+    </Tab.Navigator>
   );
 }
