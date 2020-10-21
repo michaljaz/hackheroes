c236f742e648835bc1cdf4edb887f59b51f2c744
Maciej <maciekcx11@gmail.com>
2020-10-22 01:08:38 +0200

    K O L O R K I moi drodzy

---
+++ assets/colors.js
@@ -0,0 +1,35 @@
+export const COLORS = {
+    vibrant_red: '#DD0000',
+    vibrant_green: '#00BB00',
+    vibrant_blue: '#007EFE',
+    vibrant_yellow: '#FEF601',
+
+    pastel_red: '#F24130',
+    pastel_blue: '#0540F2',
+    pastel_green: '#02732A',
+    pastel_yellow: '#F2E205',
+
+    strong_red: '#F23D4C',
+    light_pink: '#CE51DE',
+    dark_gray: '#2A2D40',
+    
+    material_red: '#EB4949',
+    material_blue: '#02B3E4',
+
+    fresh_water: '#85dc0b',
+    pale_blue: '#d0fefe',
+    sand_yellow: '#fdee73',
+    light_lime: '#aefd6c',
+    yellow_nice: '#F7EA2B',
+    peach: '#ffb07c',
+    pale_green: '#00FF40',
+    lavender: '#efc0fe',
+    hot_pink: '#FF69B4',
+    aquamarine: '#7FFFD4',
+    amaranth: '#E52B50',
+
+}
+
+// przepraszam, że poświęciłem tyle czasu na wybór kolorów
+// zbyt bardzo chcę, by to pięknie wyglądało,
+// sorry ~ cupr
